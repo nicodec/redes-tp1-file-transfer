f6e1ee982d07aa19c22c55f5a63e2ea8abc884c5
Lucas Soro <lugusor@gmail.com>
2025-04-26 22:16:12 -0300

    Refector de start-server. Corregion de errores y warning de Flake8

--- src/start_server.py
+++ src/start_server.py
@@ -5,6 +5,7 @@ import logging
 import argparse
 from threading import Thread, Event
 import queue
+from typing import Any
 from server.server_client import Client
 from message.message import TOTAL_BYTES_LENGTH, ErrorCode, Message, MessageType
 from server.udp_stop_and_wait.upload import upload_saw_server
@@ -14,6 +15,15 @@ from utils.logger import logger
 
 DEFAULT_PROTOCOL = 'udp_saw'
 MAX_FILE_SIZE = 1024 * 1024 * 100  # 100 MB
+
+
+class ServerData:
+    """Estructura que contiene el estado del server"""
+    def __init__(self):
+        self.sock = None
+        self.storage_path = ""
+        self.clients = dict[Any, Client]()
+        self.protocol = DEFAULT_PROTOCOL
 
 
 def recv_message(sock, timeout=None):
@@ -101,16 +111,24 @@ def download(sock, client_address, messages_queue,
 
 def parse_arguments():
     """Parsea los argumentos de línea de comandos"""
-    parser = argparse.ArgumentParser(description="START SERVER Description", formatter_class=CustomHelpFormatter)
+    parser = argparse.ArgumentParser(description="START SERVER Description",
+                                     formatter_class=CustomHelpFormatter)
     verbosity_group = parser.add_mutually_exclusive_group()
-    verbosity_group.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
-    verbosity_group.add_argument("-q", "--quiet", action="store_true", help="decrease output verbosity")
-    parser.add_argument("-H", "--host", metavar="ADDR", type=str, help="service IP address", default="localhost")
-    parser.add_argument("-p", "--port", type=int, help="service port", default=8888)
-    parser.add_argument("-s", "--storage", metavar="DIRPATH", type=str, help="storage dir path", 
+    verbosity_group.add_argument("-v", "--verbose", action="store_true",
+                                 help="increase output verbosity")
+    verbosity_group.add_argument("-q", "--quiet", action="store_true",
+                                 help="decrease output verbosity")
+    parser.add_argument("-H", "--host", metavar="ADDR", type=str,
+                        help="service IP address", default="localhost")
+    parser.add_argument("-p", "--port", type=int,
+                        help="service port", default=8888)
+    parser.add_argument("-s", "--storage", metavar="DIRPATH", type=str,
+                        help="storage dir path",
                         default=os.getcwd() + '/server/files')
-    parser.add_argument("-r", "--protocol", metavar="protocol", type=str, help="error recovery protocol",
-                        default=DEFAULT_PROTOCOL, choices=["udp_saw", "udp_sr"])
+    parser.add_argument("-r", "--protocol", metavar="protocol", type=str,
+                        help="error recovery protocol",
+                        default=DEFAULT_PROTOCOL,
+                        choices=["udp_saw", "udp_sr"])
 
     parser.usage = parser.format_usage()
     for a in parser._actions:
@@ -119,11 +137,67 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def process_client_message(server_data: ServerData, message: Message,
+                           client_address):
+    """Dado un mensaje proveniente del cliente ya deserializado, segun su tipo:
+    UPLOAD: Inicia un flujo de subida con el cliente
+    DOWNLOAD: Inicia un flujo de descarga con el cliente
+    ERROR, ACK, DATA, END, ACK_END: Deriva el mensaje al flujo con el cliente
+    """
+    # Crear o actualizar cliente
+    msg_type = message.get_type()
+    if (msg_type in [MessageType.UPLOAD, MessageType.DOWNLOAD] and
+            client_address in server_data.clients):
+        server_data.clients[client_address].add_message(message)
+        return
+
+    # logger.info(f"Mensaje recibido desde {client_address}: {message}")
+
+    # Procesar el mensaje según su tipo
+    if msg_type == MessageType.UPLOAD:
+        msg_file_name = message.get_file_name()
+        logger.info(f"Cliente {client_address} se ha conectado.")
+        logger.info(f"Solicitud de subida de archivo: {msg_file_name}")
+        messages_queue = queue.Queue()
+        filename = os.path.join(server_data.storage_path, msg_file_name)
+        stop_event = Event()
+        upload_worker = Thread(
+            target=upload, args=(server_data.sock, client_address, message,
+                                 messages_queue, filename, stop_event,
+                                 server_data.protocol))
+        server_data.clients[client_address] = Client(
+            client_address, upload_worker, messages_queue, stop_event)
+        server_data.clients[client_address].run()
+
+    elif msg_type == MessageType.DOWNLOAD:
+        msg_file_name = message.get_file_name()
+        logger.info(f"Cliente {client_address} se ha conectado.")
+        logger.info(f"Archivo a descargar: {msg_file_name}")
+        messages_queue = queue.Queue()
+        filename = os.path.join(server_data.storage_path, msg_file_name)
+        stop_event = Event()
+        download_worker = Thread(
+            target=download, args=(server_data.sock, client_address,
+                                   messages_queue, filename, stop_event,
+                                   server_data.protocol))
+        server_data.clients[client_address] = Client(
+            client_address, download_worker, messages_queue, stop_event)
+        server_data.clients[client_address].run()
+
+    elif msg_type in [MessageType.ERROR, MessageType.ACK, MessageType.DATA,
+                      MessageType.END, MessageType.ACK_END]:
+        if (client_address in server_data.clients):
+            server_data.clients[client_address].add_message(message)
+    else:
+        logger.error("Mensaje no reconocido.")
+
+
 def start_server():
     """Inicia el servidor UDP"""
     args = parse_arguments()
+    server_data = ServerData()
 
-    path = os.getcwd() + '/server/files'
+    server_data.storage_path = os.getcwd() + '/server/files'
 
     if args.verbose:
         logger.setLevel(logging.DEBUG)
@@ -132,69 +206,38 @@ def start_server():
     else:
         logger.setLevel(logging.INFO)
 
-    path = args.storage
-    # Crear directorio de almacenamiento si no existe
-    if not os.path.exists(path):
-        os.makedirs(path)
-        logger.info(f"Directorio de almacenamiento creado: {path}")
+    if args.protocol is not None:
+        server_data.protocol = args.protocol
 
-    protocol = args.protocol if args.protocol else DEFAULT_PROTOCOL
+    if args.storage is not None:
+        server_data.storage_path = args.storage
+
+    # Crear directorio de almacenamiento si no existe
+    if not os.path.exists(server_data.storage_path):
+        os.makedirs(server_data.storage_path)
+        logger.info(f"Directorio de almacenamiento creado: \
+                    {server_data.storage_path}")
 
     # Crear socket UDP
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_data.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = (args.host, args.port)
-    sock.bind(server_address)
+    server_data.sock.bind(server_address)
 
     logger.info(f"Servidor iniciado en: {server_address}")
-    logger.info(f"Almacenamiento: {path}")
+    logger.info(f"Almacenamiento: {server_data.storage_path}")
     logger.info(f"Protocolo: {args.protocol}")
 
     # Diccionario para mantener los clientes conectados
-    clients = {}
+    clients = dict[Any, Client]()
 
     try:
         logger.info("Esperando mensajes...")
         while True:
             # Recibir mensaje
-            message, client_address = recv_message(sock)
+            message, client_address = recv_message(server_data.sock)
 
             if message:
-                # Crear o actualizar cliente
-                if (message.get_type() == MessageType.UPLOAD or message.get_type() == MessageType.DOWNLOAD) and client_address in clients:
-                    clients[client_address].add_message(message)
-                    continue
-
-                # logger.info(f"Mensaje recibido desde {client_address}: {message}")
-
-                # Procesar el mensaje según su tipo
-                if message.get_type() == MessageType.UPLOAD:
-                    logger.info(f"Cliente {client_address} se ha conectado.")
-                    logger.info(f"Solicitud de subida de archivo: {message.get_file_name()}")
-                    messages_queue = queue.Queue()
-                    filename = os.path.join(path, message.get_file_name())
-                    stop_event = Event()
-                    upload_worker = Thread(target=upload,
-                                           args=(sock, client_address, message,
-                                                 messages_queue, filename, 
-                                                 stop_event, protocol))
-                    clients[client_address] = Client(client_address, upload_worker, messages_queue, stop_event)
-                    clients[client_address].run()
-
-                elif message.get_type() == MessageType.DOWNLOAD:
-                    logger.info(f"Cliente {client_address} se ha conectado.")
-                    logger.info(f"Archivo a descargar: {message.get_file_name()}")
-                    messages_queue = queue.Queue()
-                    filename = os.path.join(path, message.get_file_name())
-                    stop_event = Event()
-                    download_worker = Thread(target=download, args=(sock, client_address, messages_queue, filename, stop_event, protocol))
-                    clients[client_address] = Client(client_address, download_worker, messages_queue, stop_event)
-                    clients[client_address].run()
-
-                elif message.get_type() == MessageType.ERROR or message.get_type() == MessageType.ACK or message.get_type() == MessageType.DATA or message.get_type() == MessageType.END or message.get_type() == MessageType.ACK_END:
-                    if (client_address in clients):
-                        clients[client_address].add_message(message)
-                else:
-                    logger.error("Mensaje no reconocido.")
+                process_client_message(server_data, message, client_address)
 
             # Verificar clientes inactivos
             clients_to_remove = []
@@ -210,7 +253,7 @@ def start_server():
     except KeyboardInterrupt:
         logger.info("Deteniendo servidor...")
         logger.info(f"Desconectando {len(clients)} clientes activos")
-        sock.close()
+        server_data.sock.close()
         logger.info("Servidor detenido.")
 
 
