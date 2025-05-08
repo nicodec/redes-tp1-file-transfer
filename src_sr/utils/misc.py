21fea8778466a1e3a7cce0c457c27f401428eadd
Maximiliano Prystupiuk <mprystupiuk@fi.uba.ar>
2025-05-08 13:18:03 -0300

    Achicado data a 2 fragmentos

--- src_sr/utils/misc.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-
-class CustomHelpFormatter(argparse.HelpFormatter):
-    def _format_action_invocation(self, action):
-        if action.metavar == '\b':
-            return ', '.join(action.option_strings)
-        return super()._format_action_invocation(action)
